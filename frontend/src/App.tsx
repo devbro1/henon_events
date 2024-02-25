0f311675c36e880e30057724cf18744ec0d7adb2
Farzad Meow Khalafi <farzadk@gmail.com>
2024-02-25 23:58:56 +0000

    fix menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense, useContext, useEffect } from 'react';
 import { useState } from 'react'
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css'
 
 const AddEventComp = React.lazy(() => import('./comps/addEvent'));
@@ -8,18 +8,18 @@ const EventsListComp = React.lazy(() => import('./comps/eventsList'));
 const EventsTimelineComp = React.lazy(() => import('./comps/eventsTimeline'));
 
 function App() {
-  const selected = "border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white";
+  const location = useLocation();
+  console.log(location.pathname.indexOf(''));
+  const selected = "inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white";
   const not_selected = "inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3";
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      
       <div className="">
-          <div className={selected} ><Link to={'/add-event/'}>Add Event</Link></div>
-          <div className={not_selected} ><Link to={'/events-list/'}>Events List</Link></div>
-          <div className={not_selected} ><Link to={'/events-timeline/'}>Events Timeline</Link></div>
+        <ul className="flex">
+          <li className="mr-3"><Link to={'/add-event/'} className={location.pathname.indexOf('add-event') > 0 ? selected: not_selected}>Add Event</Link></li>
+          <li className="mr-3"><Link to={'/events-list/'} className={location.pathname.indexOf('events-list') > 0 ? selected: not_selected}>Events List</Link></li>
+          <li className="mr-3"><Link to={'/events-timeline/'} className={location.pathname.indexOf('events-timeline') > 0 ? selected: not_selected}>Events Timeline</Link></li>
+        </ul>
       </div>
       <div>
         <Routes>
